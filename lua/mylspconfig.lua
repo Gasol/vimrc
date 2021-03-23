1049639d993639f21fb79319bad0a7ef54050107
Gasol Wu <gasol.wu@gmail.com>
2021-03-23 14:50:18 +0800

    refactor: Use local variables

--- lua/mylspconfig.lua
+++ lua/mylspconfig.lua
@@ -1,12 +1,13 @@
 vim.lsp.set_log_level('info')
-default_available_servers = {
+
+local default_available_servers = {
 	'pyright',
 	'intelephense',
 	'vuels',
 	'tsserver',
 }
 
-status, lspconfig = pcall(require, 'lspconfig')
+local status, lspconfig = pcall(require, 'lspconfig')
 if status then
 	for i, name in ipairs(default_available_servers) do
 		lspconfig[name].setup{}
